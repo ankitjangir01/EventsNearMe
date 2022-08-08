7a61c67e39e16020a515a3d2e4c968fe086d8119
Ankit Jangir <ankitjangirnice@gmail.com>
2022-08-08 17:08:19 +0530

    revision

--- frontend/src/Components/Event.js
+++ frontend/src/Components/Event.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 const Event = (props) => {
-  const {_id, poster, title, address, city, state, country, date } = props.event;
+  const { _id, poster, title, address, city, state, country, date } = props.event;
 
   const onClickShareBtn = () => {
     let url = 'http://events-near-me-india.herokuapp.com/eventdetails';
@@ -12,7 +12,7 @@ const Event = (props) => {
   }
 
   return (
-    <div className="row col-md-5 event-card mx-4 my-4 shadow-lg">
+    <div className="col-md-4 event-card mx-4 my-4 shadow-lg">
       <img src={poster} className="img-fluid rounded my-2" alt="no event poster available" />
       <div className="p-4">
         <div className="card-body">
@@ -22,15 +22,15 @@ const Event = (props) => {
           <p className="card-text"><i className="fa-solid fa-calendar-days" /> {date}</p>
           <p className="card-text"><i className="fa-solid fa-clock" /> 5 days, 5 hrs to go</p>
         </div>
-        <hr />
+        <hr/>
       </div>
       <div className="px-4 d-flex justify-content-between">
-        <button type="button" className="btn btn-sm btn-outline-dark"><i className="fa-solid fa-circle-info" />
-          <Link to={`/eventdetails/${_id}`} style={{ color: 'inherit', textDecoration: 'inherit' }}> More Details </Link>
+        <button type="button" className="btn btn-sm btn-outline-dark"><i className="fa-solid fa-circle-info"/>
+           <Link to={`/eventdetails/${_id}`} style={{ color: 'inherit', textDecoration: 'inherit'}}> More Details </Link>
         </button>
         <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button type="button" className="btn btn-outline-dark" onClick={onClickShareBtn}><i className="fa-solid fa-share-nodes" /> Share</button>
-          <button type="button" className="btn btn-outline-dark"><i className="fa-solid fa-bookmark" /> Save</button>
+          <button type="button" className="btn btn-outline-dark" onClick={onClickShareBtn}><i className="fa-solid fa-share-nodes"/> Share</button>
+          <button type="button" className="btn btn-outline-dark"><i className="fa-solid fa-bookmark"/> Save</button>
         </div>
       </div>
     </div>
